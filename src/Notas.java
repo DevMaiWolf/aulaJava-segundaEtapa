4d0966e85c236fb34f9d87f86568291d473c6d04
Maiara Viegas <111406385+DevMaiWolf@users.noreply.github.com>
2023-09-29 22:49:35 -0300

    exercicio 1 de encapsulamento visibilidade

---
+++ src/Notas.java
@@ -0,0 +1,28 @@
+class Nota {
+    private double nota1;
+    private double nota2;
+
+    // construtor da class Nota
+    public Nota(double nota1, double nota2) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    // métodos get e set para  nota1 e nota2
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+}
+
